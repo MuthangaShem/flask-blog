51e919f8e5fa3a26a8a4cec3264662410f28f4c7
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-04-23 13:16:01 +0300

    add posts route

--- flaskbog/views.py
+++ flaskbog/views.py
@@ -11,3 +11,15 @@ from config import PER_PAGE, MAX_SEARCH_RESULTS
 def index():
     return render_template('index.html')
 
+
+@app.route('/posts', defaults={'page': 1})
+@app.route('/posts/page/<int:page>')
+def posts(page):
+    count = db.session.query(Post).count()
+    offset = (page - 1) * PER_PAGE
+    posts = Post.query.limit(PER_PAGE).offset(offset)
+
+    if not posts and page != 1:
+        abort(404)
+    pagination = Pagination(page, PER_PAGE, count)
+    return render_template('posts.html', pagination=pagination, posts=posts)
