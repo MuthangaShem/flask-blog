7a823888f18669155300f1a6fcef1c518a233ab9
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-04-23 13:14:24 +0300

    add index route in views.py

--- flaskbog/views.py
+++ flaskbog/views.py
@@ -4,3 +4,10 @@ from form import LoginForm, PostForm, TagForm, SearchForm
 from models import db, Admin, Tag, Post, Pagination
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from config import PER_PAGE, MAX_SEARCH_RESULTS
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
