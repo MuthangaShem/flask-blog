802c1803a19dfbfe5afa8b5862fcd302dff37df4
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-04-23 15:05:17 +0300

    add and edit loginform class

---
+++ flaskbog/form.py
@@ -0,0 +1,23 @@
+from flask.ext.wtf import Form, TextField, TextAreaField, SelectField, SelectMultipleField, SubmitField, validators, ValidationError, PasswordField
+from models import db, Admin
+
+
+class LoginForm(Form):
+    admin = TextField('Admin', [validators.Required()])
+
+    def __init__(self, *args, **kwargs):
+        kwargs['csrf_enabled'] = False
+        Form.__init__(self, *args, **kwargs)
+
+    def validate(self):
+        lf = Form.validate(self)
+        if not lf:
+            return False
+
+        admin = Admin.query.filter_by(userd=self.admin.data).first()
+        if admin is None:
+            self.admin.errors.append('Unknown admin')
+            return False
+
+        self.admin = admin
+        return True
